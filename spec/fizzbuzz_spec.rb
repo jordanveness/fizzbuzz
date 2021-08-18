83834827fc5acf9eaa7f8b0c17d83f650aab3d1f
EC2 Default User <ec2-user@ip-172-31-45-169.us-east-2.compute.internal>
2021-08-18 16:07:23 +0000

    Functioning fizzbuzz program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,12 +1,17 @@
 require './lib/fizzbuzz'
 
 describe "fizzbuzz" do
+    it 'returns "fizzbuzz" when the number is divisible by both 3 and 5' do
+      fizz_15 = fizzbuzz(15)
+      
+      expect(fizz_15).to eq("fizzbuzz")
+    end 
     it 'returns "fizz" when the number is divisible by 3' do
       fizz_3 = fizzbuzz(3)
       
       expect(fizz_3).to eq("fizz")
     end
-    it 'return "buzz" when the number is divisible by 5' do
+    it 'returns "buzz" when the number is divisible by 5' do
       fizz_5 = fizzbuzz(5)
       
       expect(fizz_5).to eq("buzz")
