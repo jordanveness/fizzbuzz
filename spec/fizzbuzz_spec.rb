f11b240b0a012c59c6fc5a209041cf11f25e5f86
EC2 Default User <ec2-user@ip-172-31-45-169.us-east-2.compute.internal>
2021-08-18 15:58:27 +0000

    adding a method that returns buzz if divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,9 @@ describe "fizzbuzz" do
       
       expect(fizz_3).to eq("fizz")
     end
+    it 'return "buzz" when the number is divisible by 5' do
+      fizz_5 = fizzbuzz(5)
+      
+      expect(fizz_5).to eq("buzz")
+    end 
 end
